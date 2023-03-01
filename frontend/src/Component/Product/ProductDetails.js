0aa414f15be3693e898fea0b8e6828933aa9efdc
BibekRajPaudel <paudelrajbibek@gmail.com>
2023-03-01 22:29:33 +0545

    cart action and reducer

--- frontend/src/Component/Product/ProductDetails.js
+++ frontend/src/Component/Product/ProductDetails.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import Carousel from "react-material-ui-carousel";
 import "./ProductDetails.css";
 import { useSelector, useDispatch } from "react-redux";
@@ -9,6 +9,7 @@ import ReviewCard from "./ReviewCard";
 import Loader from "../Layout/Loader/Loader";
 import { useAlert } from "react-alert";
 import MetaData from "../Layout/metadata";
+import { addItemsToCart } from "../../actions/cartAction";
 
 const ProductDetails = ({ match }) => {
   const { id } = useParams()
@@ -33,6 +34,27 @@ const ProductDetails = ({ match }) => {
     size: window.innerWidth < 600 ? 20 : 25,
     value: product.ratings,
     isHalf: true,
+  };
+
+  const [quantity, setQuantity] = useState(1);
+
+  const increaseQuantity = () => {
+    if (product.Stock <= quantity) return;
+
+    const qty = quantity + 1;
+    setQuantity(qty);
+  };
+
+  const decreaseQuantity = () => {
+    if (1 >= quantity) return;
+
+    const qty = quantity - 1;
+    setQuantity(qty);
+  };
+
+  const addToCartHandler = () => {
+    dispatch(addItemsToCart(id, quantity));
+    alert.success("Item Added To Cart");
   };
 
   return (
@@ -71,11 +93,11 @@ const ProductDetails = ({ match }) => {
             <h1>{`Rs${product.price}`}</h1>
             <div className="detailsBlock-3-1">
               <div className="detailsBlock-3-1-1">
-                <button>-</button>
-                <input readOnly type="number" value={1} />
-                <button>+</button>
+              <button onClick={decreaseQuantity}>-</button>
+                    <input readOnly type="number" value={quantity} />
+                    <button onClick={increaseQuantity}>+</button>
               </div>
-              <button>Add to Cart</button>
+              <button onClick={addToCartHandler}>Add to Cart</button>
             </div>
 
             <p>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,23 +1,37 @@
-import { createStore , combineReducers, applyMiddleware } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-import { productsReducer, productDetailsReducer } from "./reducer/productReducer";
-import { forgotPasswordReducer, profileReducer, userReducer } from "./reducer/userReducer";
+import {
+  productsReducer,
+  productDetailsReducer,
+} from "./reducer/productReducer";
+import {
+  forgotPasswordReducer,
+  profileReducer,
+  userReducer,
+} from "./reducer/userReducer";
+import { cartReducer } from "./reducer/cartReducer";
 
 const reducer = combineReducers({
-  products:productsReducer,
-  productDetails:productDetailsReducer,
-  user:userReducer,
-  profile:profileReducer,
-  forgotPassword:forgotPasswordReducer
-
+  products: productsReducer,
+  productDetails: productDetailsReducer,
+  user: userReducer,
+  profile: profileReducer,
+  forgotPassword: forgotPasswordReducer,
+  cart: cartReducer,
 });
 
-let initialState = {};
+let initialState = {
+  cart: {
+    cartItems: localStorage.getItem("cartItems")
+      ? JSON.parse(localStorage.getItem("cartItems"))
+      : [],
+  },
+};
 
 const middleware = [thunk];
 
-const store = createStore (
+const store = createStore(
   reducer,
   initialState,
   composeWithDevTools(applyMiddleware(...middleware))
